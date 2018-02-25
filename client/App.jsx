fc03492859d3ab382e9252cae74c152bd451aae0
Melissa Mepham <m.a.mepham@gmail.com>
2018-02-25 14:31:19 +1300

    got p5 working nodemon isnt

--- client/App.jsx
+++ client/App.jsx
@@ -1,18 +1,33 @@
 
 import React from 'react'
-import {render} from 'react-dom'
-import {HashRouter as Router, Route} from 'react-router-dom'
+import ReactDOM from 'react-dom'
 import P5Wrapper from 'react-p5-wrapper'
-import Sketch from './components/Sketch'
+import sketch from './sketches/sketch'
 
-const App = (props) => {
+import {HashRouter as Router, Route} from 'react-router-dom'
+
+
+class App extends React.Component{
+constructor(props) {
+  super(props);
+  this.state = {
+    rotation: 150,
+    stateSketch: sketch,
+  };
+}
+render() {
+
   return (
     <Router>
+      <div>
       <div>Hello World</div>
-      <P5Wrapper sketch={Sketch} />
+        <P5Wrapper sketch={this.state.stateSketch}/>
+        </div>
     </Router>
 
+
   )
+}
 }
 
   export default App
